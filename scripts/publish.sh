bc4dc3910cb0e7260d2457e9533a54baf71170d9
bino <bonoself@gmail.com>
2024-09-05 03:58:35 +0900

    chore(publish.sh): build when prepublish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd ./packages/$package && npm version patch && npx pkg-to-dist && cd dist && pnpm publish --no-git-checks
+cd ./packages/$package && pnpm build && npm version patch && npx pkg-to-dist && cd dist && pnpm publish --no-git-checks
