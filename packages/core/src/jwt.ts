99fb8dc6b92ebbb135b4213dc06e3a21e2ff8cff
bino <bonoself@gmail.com>
2024-10-09 19:30:59 +0900

    fix(jwt): support ESM & CJS both

--- packages/core/src/jwt.ts
+++ packages/core/src/jwt.ts
@@ -1,19 +1,20 @@
 import { TokenManagerParams, SessionTokens, TokenAdapter } from '@tytan-auth/common';
-import * as jwt from 'jsonwebtoken';
+import * as jsonwebtoken from 'jsonwebtoken';
 import type { JwtPayload } from 'jsonwebtoken';
 class JwtAdapter<T extends object> implements TokenAdapter<T> {
     private readonly secret: string;
+    private readonly jwt = ('default' in jsonwebtoken) ? jsonwebtoken.default as typeof jsonwebtoken : jsonwebtoken;
     constructor({
         secret,
     }: TokenManagerParams) {
         this.secret = secret;
     }
     async generate(payload: T, expiresIn: number): Promise<string> {
-        return jwt.sign(payload, this.secret, { expiresIn });
+        return this.jwt.sign(payload, this.secret, { expiresIn });
     }
     async verify(token: string): Promise<T & JwtPayload> {
         return new Promise((resolve, reject) => {
-            jwt.verify(
+            this.jwt.verify(
                 token,
                 this.secret,
                 (err, decoded) => err ? reject(err) : resolve(decoded as T & JwtPayload)
